0535bed4e3d7c5d57ccb217666e0f5f6a2f42f56
unknown <florent.tata91@gmail.com>
2023-04-03 11:42:08 +0300

    for commit

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,17 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router_user = express.Router()
+
+require('dotenv').config()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+router_user.get('/', function(req, res, next) {
+  res.render('pages/index-3', 
+    { 
+      title: 'Switch-gaming', 
+      menus: JSON.parse(JSON.stringify(process.env.MENUS)).split(','),
+
+    });
 });
 
-router.get('/products', function(req, res, next) {
-  res.render('pages/shop-fullwidth', { title: 'Express' });
+router_user.get('/products', function(req, res, next) {
+  res.render('pages/shop-grid-2', 
+    { 
+      title: 'Switch-gaming', 
+      menus: JSON.parse(JSON.stringify(process.env.MENUS)).split(','),
+    });
 });
 
-router.get('/vendors', function(req, res, next) {
-  res.render('pages/shop-vendor-list', { title: 'Express' });
+router_user.get('/vendors', function(req, res, next) {
+  res.render('pages/shop-vendor-list', 
+    { 
+      title: 'Switch-gaming', 
+      menus: JSON.parse(JSON.stringify(process.env.MENUS)).split(','),
+    });
 });
 
-module.exports = router;
+module.exports = router_user;
